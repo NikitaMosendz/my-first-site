69c459a52aae97825bee4f2d48e0eb351b3aabfc
NikitaMosendz <nikitaworkmosendz@gmail.com>
2026-06-09 20:59:55 +0300

    Seventh commit: raised click counter logic higher, and made it in 1 line

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const clickCounter = document.getElementById('click-counter');
 let clicks = 0;
 
 button.addEventListener('click', () => {
+    clickCounter.textContent = `Clicks: ${++clicks}`
+    
     if (body.classList.toggle('light-theme')) {
         button.textContent = 'Dark theme'
     } else {
         button.textContent = 'Light theme'
     }
-    clicks++;
-    clickCounter.textContent = `Clicks: ${clicks}`
 })
